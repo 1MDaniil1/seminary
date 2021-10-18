209881784e8e715fbbcfb0851abec5d8fa5b8f3d
1MDaniil1 <mr.daniilmurzaev55@mail.ru>
2021-10-18 16:27:34 +0300

    Changed

--- lab4(catch the ball)/game.py
+++ lab4(catch the ball)/game.py
@@ -14,57 +14,66 @@ MAGENTA = (255, 0, 255)
 CYAN = (0, 255, 255)
 BLACK = (0, 0, 0)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
-
+j=0
 def new_ball():
-    global x, y, r, color
+    global x, y, r, color, j
     x = randint(100,1100)
     y = randint(100,600)
     r = randint(30,50)
+    j+=1
     color = COLORS[randint(0, 5)]
     circle(screen, color, (x, y), r)
-    ballsc.append(tuple((x,y)))
+    ballsc.append([x,y])
     ballsr.append(r)
+    bcolours.append(color)
     c()
 
 ballsv=[]
 ballsc=[]
 ballsr=[]
-    
+bcolours=[]
+
 def click(event):
-    for i in range(len(ballsc)):
-        l=ballsc[i]
-        if (l[0]-event.pos[0])**2+(l[1]-event.pos[1])**2<=(ballsr[i])**2:
-            circle(screen, (0, 0, 0), (l[0],l[1]), r)
-            new_ball()
+    global j, score
+    for i in range(j):
+        if (ballsc[i][0]-event.pos[0])**2+(ballsc[i][1]-event.pos[1])**2<=(ballsr[i])**2:
+            circle(screen, (50, 50, 50), (ballsc[i]), ballsr[i])
+            score+=1
+            ballsc[i]=[randint(100,1100),randint(100,600)]
+            ballsr[i]=randint(30,50)
+            ballsv[i]=[randint(-5,5),randint(-5,5)]
+            bcolours[i]=COLORS[randint(0, 5)]
+            break
    
-    
+
 
 pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
 
 def c():
-    global dx, dy
-    dx=randint(-10,10)
-    dy=randint(-10,10)
-    ballsv.append(tuple((dx,dy)))
+    ballsv.append([randint(-5,5),randint(-5,5)])
     
 def move(screen):
-    global x, y, dx, dy
-    x+=dx
-    y+=dy
-    if x+r>=1200 or x-r<=0:
-        dx=-dx
-    if y+r>=700 or y-r<=0:
-        dy=-dy
-    circle(screen, color, (x, y), r)
- 
+    for i in range(j):
+        if ballsc[i][0]+ballsr[i]>=1200 or ballsc[i][0]-ballsr[i]<=0:
+            ballsv[i][0]=-ballsv[i][0]       
+        if ballsc[i][1]+ballsr[i]>=700 or ballsc[i][1]-ballsr[i]<=0:
+            ballsv[i][1]=-ballsv[i][1]
+        ballsc[i][0]+=ballsv[i][0]
+        ballsc[i][1]+=ballsv[i][1]
+        circle(screen, (0,0,0), (ballsc[i][0], ballsc[i][1]), ballsr[i])
+        circle(screen, bcolours[i], (ballsc[i][0], ballsc[i][1]), ballsr[i])
 
-new_ball()
+
+score=0
+for i in range(5):
+    new_ball()
+    
+FONT = pygame.font.Font(None, 50)
 
 while not finished:
     clock.tick(FPS)
-    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
@@ -72,7 +81,12 @@ while not finished:
             click(event)
 
     move(screen)
+  
+ 
+
+    score_display = FONT.render(str(score), True, (255, 255, 255))
+    screen.blit(score_display, (10, 10))
     pygame.display.update()
     screen.fill(BLACK)
-
+    
 pygame.quit()
